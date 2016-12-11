3036b2f2af334100c1c50df6bf2c45dd76de46bf
Trent Oswald <trentoswald@therebelrobot.com>
2016-12-11 13:55:44 -0800

    feat(controllers): added contact pages

---
+++ controllers/contact/getContact.js
@@ -0,0 +1,20 @@
+var nodemailer = require('nodemailer')
+module.exports = {
+  method: 'get',
+  endpoint: '/contact',
+  middleware: [],
+  controller: getContact
+}
+
+function getContact (req, res) {
+  var Users = req.models.Users
+  Users.find({$or: [{'roles.core': true}, {'roles.coreLead': true}, {'roles.superAdmin': true}]}, function (err, foundUsers) {
+    if (err) console.error(err)
+    res.render(res.locals.brigade.theme.slug + '/views/contact/index', {
+      view: 'contact',
+      users: foundUsers,
+      title: 'Contact',
+      brigade: res.locals.brigade
+    })
+  }).sort({'profile.contactpagerank': 1})
+}
---
+++ controllers/contact/postContact.js
@@ -0,0 +1,39 @@
+var nodemailer = require('nodemailer')
+module.exports = {
+  method: 'post',
+  endpoint: '/contact',
+  middleware: [],
+  controller: postContact
+}
+
+function postContact (req, res) {
+  var Users = req.models.Users
+  var uniquecheck = {}
+  for (var key in req.body) {
+    if (req.body[key].contactrank) {
+      if (!uniquecheck[req.body[key].contactrank]) {
+        uniquecheck[req.body[key].contactrank] = 'present'
+      } else {
+        req.flash('errors', {msg: 'Changes failed. Please enter unique values for contact page ranks'})
+        res.redirect('/contact/edit')
+        return
+      }
+    }
+  }
+  Users.find({$or: [{'roles.core': true}, {'roles.coreLead': true}, {'roles.superAdmin': true}]}, function (err, foundUsers) {
+    if (err) console.error(err)
+    foundUsers.forEach(function (user) {
+      var thisUser = new Users(user)
+      thisUser.profile.contactpagerank = req.body[thisUser.username].contactrank
+      if (req.body[thisUser.username].showcontact) {
+        thisUser.profile.showcontact = true
+      } else {
+        thisUser.profile.showcontact = false
+      }
+      thisUser.save(function (err) {
+        if (err) console.error(err)
+      })
+    })
+  })
+  res.redirect('/contact/edit')
+}
