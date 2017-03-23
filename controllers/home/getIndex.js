74d8e429b8d5ce8a2662dafdce73655101f14888
Perry Taylor <ptaylor@hmsracing.com>
2017-03-23 15:15:18 -0400

    Add a null check to the project update logic.

--- controllers/home/getIndex.js
+++ controllers/home/getIndex.js
@@ -30,8 +30,11 @@ function getIndex (req, res) {
     .then((results) => getProjects(Projects, results, ctx))
     .then((results) => getPosts(Posts, results, ctx))
     .then(({ foundEvents, currentEvents, allKeywords, foundProjects, foundPosts, posts }) => {
+      // adjust project count to reflect active projects
       var projectDisplay = _.find(res.locals.brigade.displayedstats, {'caption': 'Active Projects'})
-      projectDisplay.stat = foundProjects.length
+      if (!projectDisplay) {
+        projectDisplay.stat = foundProjects.length
+      }
 
       res.render(res.theme.public + '/views/home', {
         view: 'home',
